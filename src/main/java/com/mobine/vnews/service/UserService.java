baa819269fcce69a64b224029315c7cb38d0f4b1
wzes <793890838@qq.com>
2017-12-09 14:47:17 +0800

    first commit

---
+++ src/main/java/com/mobine/vnews/service/UserService.java
@@ -0,0 +1,49 @@
+package com.mobine.vnews.service;
+
+
+import com.mobine.vnews.mapper.UserMapper;
+import com.mobine.vnews.module.BasicResponse;
+import com.mobine.vnews.module.bean.User;
+
+import com.mobine.vnews.util.IdUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Create by xuantang
+ * @date on 12/9/17
+ */
+@Service
+public class UserService {
+
+    @Autowired
+    UserMapper userMapper;
+    /**
+     * 日志输出
+     */
+    private static Logger log = LogManager.getLogger(UserService.class);
+
+    public BasicResponse<String> register(User user) {
+        BasicResponse<String> response = new BasicResponse<>();
+        int code = 200;
+        String message = "success";
+        try {
+            // add user
+            user.setID(IdUtils.getUUID());
+            System.out.println(user.toString());
+            int res = userMapper.addUser(user);
+            log.info(res);
+        } catch (Exception e) {
+            code = 400;
+            message = e.getMessage();
+        }
+        // set values
+        response.setCode(code);
+        response.setMessage(message);
+        // log
+        log.info(response.toString());
+        return response;
+    }
+}

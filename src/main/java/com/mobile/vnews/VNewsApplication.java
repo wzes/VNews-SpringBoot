1a9e0e8c9c23396591d1ca6cda4d44b36943da22
Anifled <1196479790@qq.com>
2017-12-19 14:03:38 +0800

    验证能否用sdk

--- src/main/java/com/mobile/vnews/VNewsApplication.java
+++ src/main/java/com/mobile/vnews/VNewsApplication.java
@@ -1,5 +1,6 @@
 package com.mobile.vnews;
 
+import com.mobile.vnews.util.SmsSender;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VNewsApplication {
 
 	public static void main(String[] args) {
+		SmsSender smsSender=new SmsSender();
+		try{
+			smsSender.sendMessage();
+		}catch (Exception e){
+			e.getMessage();
+		}
 		SpringApplication.run(VNewsApplication.class, args);
 	}
 }

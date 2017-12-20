5e0ec0ae62b949d07565a8369f338d5b625339bd
wzes <793890838@qq.com>
2017-12-20 16:55:34 +0800

    change notice class name to message

--- src/main/java/com/mobile/vnews/service/NoticeService.java
+++ src/main/java/com/mobile/vnews/service/NoticeService.java
@@ -5,27 +5,27 @@ package com.mobile.vnews.service;
 *@Date:11:33 2017/12/14
 */
 import com.mobile.vnews.module.BasicResponse;
-import com.mobile.vnews.mapper.NoticeMapper;
-import com.mobile.vnews.module.bean.Notice;
+import com.mobile.vnews.mapper.MessageMapper;
+import com.mobile.vnews.module.bean.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class NoticeService{
     @Autowired
-    NoticeMapper noticeMapper;
+    MessageMapper messageMapper;
 
     /**
      *
      * @param user_id
      * @return
      */
-    public BasicResponse<List<Notice>> getNoticeByID(String user_id) {
-        BasicResponse<List<Notice>> response = new BasicResponse<>();
+    public BasicResponse<List<Message>> getMessagesByID(String user_id) {
+        BasicResponse<List<Message>> response = new BasicResponse<>();
         int code = 200;
         String message = "get notice by user ID success";
         try{
-            List<Notice> notice = noticeMapper.getNoticesByUserID(user_id);
+            List<Message> notice = messageMapper.getMessagesByUserID(user_id);
             if(notice.isEmpty()) {
                 code = 400;
                 message = "didn't have notice";
